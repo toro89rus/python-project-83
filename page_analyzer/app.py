6cd24eea1045deffd109eb0959d187434af7661b
toro <timofey89rus@gmail.com>
2025-02-25 15:35:56 +0300

    add db mock for deploy testing

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,18 @@
 import os
+import psycopg2
 
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 load_dotenv()
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-
+DATABASE_URL = os.getenv("DATABASE_URL")
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route("/")
 def index():
+    if conn:
+        return "SQL connected successfully"
     return render_template("main.html"), 200

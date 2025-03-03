0cc429ad9b41cbf244c8fc4dad0f2c47768f4e28
toro <timofey89rus@gmail.com>
2025-03-03 16:36:07 +0300

    fix html_parser module

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -4,10 +4,9 @@ from bs4 import BeautifulSoup
 def get_seo_content(html):
     soup = BeautifulSoup(html, "html.parser")
     title = soup.title.contents[0] if soup.title else ""
-    h1 = soup.h1.contents[0] if soup.h1 else ""
-    content = soup.find("meta", attrs={"name": "description"}).get(
-        "content", ""
-    )
+    h1 = soup.h1.text if soup.h1 else ""
+    description = soup.find("meta", attrs={"name": "description"})
+    content = description.get("content", "") if description else ""
     return {
         "h1": h1,
         "title": title,

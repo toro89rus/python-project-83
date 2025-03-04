ecb73d42ad926187b5c6e38212b8e24b35f3a97d
toro <timofey89rus@gmail.com>
2025-03-04 11:35:04 +0300

    move URL_MAX_LEN to config.py

--- page_analyzer/config.py
+++ page_analyzer/config.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
+URL_MAX_LEN = 255
+
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
--- page_analyzer/url_validator.py
+++ page_analyzer/url_validator.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 
 import validators
 
-MAX_LENGTH = 255
+from page_analyzer.config import URL_MAX_LEN
 
 
 def normalize_url(url):
@@ -14,6 +14,6 @@ def validate_url(url):
     errors = {}
     if not validators.url(url):
         errors["Incorrect URL"] = True
-    if len(url) > MAX_LENGTH:
+    if len(url) > URL_MAX_LEN:
         errors["Exceeds max length"] = True
     return errors
